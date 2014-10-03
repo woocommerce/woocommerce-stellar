5daf618b36452e5ed4e907cbf58eeca415fe98cf
thenbrent <thenbrent@gmail.com>
2014-10-04 00:37:30 +1000

    Remove confirmation check loop
    Fixes #13

--- assets/js/verify-stellar.js
+++ assets/js/verify-stellar.js
@@ -3,19 +3,10 @@ jQuery(document).ready(function($){
 	$(document).on( 'click', '.stellar-confirm', function( event ) {
 		event.preventDefault();
 
-		var time_window = wc_stellar_js.time_window;
-		var retries     = wc_stellar_js.retries;
-		var sec         = time_window;
+		$('.stellar-confirm').attr('disabled','disabled');
+		$('.stellar-payment-instructions, .stellar-transaction').slideUp();
+		$('.stellar-status').slideDown();
 
-		$('.stellar-status').show();
-
-		var timer = setInterval(function() {
-			$('.stellar-countdown').text(sec--);
-			if(sec == -1) {
-				$('.stellar-countdown').fadeOut('fast');
-				clearInterval(timer);
-			}
-		}, 1000);
 		var ajaxCall = $.ajax({
 			type: "POST",
 			url: wc_stellar_js.ajax_url,
@@ -23,28 +14,18 @@ jQuery(document).ready(function($){
 				action: 'confirm_stellar_payment',
 				order_id: wc_stellar_js.order_id
 			},
-			tryCount : 0,
-			retryLimit : retries,
 			success: function (response) {
 				response = $.parseJSON( response );
 				if( response.result === 'success' ) {
 					$('.stellar-transaction.success').show();
+					$('.stellar-transaction-pending').slideUp();
+					$('.stellar-pay-button, .stellar-payment-instructions').hide();
+					$('.stellar-confirm').hide();
 					return;
 				} else {
-					this.tryCount++;
-
-					if( this.tryCount <= this.retryLimit ) {
-						$.ajax(this);
-						$('.stellar-retries').show().text(this.tryCount);
-					}
-
-					if( this.tryCount > this.retryLimit ) {
-						$('.stellar-transaction.failed').show();
-					}
-
-					$(timer);
-					setInterval(ajaxCall, 1000 * time_window); //call itself
-
+					$('.stellar-confirm').removeAttr('disabled');
+					$('.stellar-transaction.failed').show();
+					$('.stellar-payment-instructions').slideDown();
 					return;
 				}
 			}
