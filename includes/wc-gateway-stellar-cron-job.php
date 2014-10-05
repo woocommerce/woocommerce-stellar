86cc99deb454494b69153b5f04b6ca1b78cc4c24
thenbrent <thenbrent@gmail.com>
2014-10-05 14:57:56 +1000

    Move woocommerce_stellar_cron_job() to WC_Stellar
    Part of #11

--- includes/wc-gateway-stellar-cron-job.php
+++ includes/wc-gateway-stellar-cron-job.php
@@ -1,18 +1,1 @@
 <?php
-
-function woocommerce_stellar_cron_job() {
-
-	// Fetch recent orders.
-	$order_ids = get_recent_stellar_orders();
-	// Fetch Stellar Gateway settings.
-	$stellar_settings = get_option( 'woocommerce_stellar_settings' );
-
-	$wallet_address = $stellar_settings['account_address'];
-
-	foreach ( $order_ids as $order_id ) {
-		WC_Stellar()->validate_stellar_payment( $order_id );
-	}
-
-}
-add_action( 'woocommerce_stellar_cron_job', 'woocommerce_stellar_cron_job' );
-
--- woocommerce-stellar-gateway.php
+++ woocommerce-stellar-gateway.php
@@ -150,6 +150,8 @@ final class WC_Stellar {
 
 		add_action( 'woocommerce_view_order', array( $this, 'stellar_instructions' ), 11, 1 );
 
+		add_action( 'woocommerce_stellar_cron_job', array( $this, 'woocommerce_stellar_cron_job' ), 10, 1 );
+
 		// Is WooCommerce activated?
 		if( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 			deactivate_plugins( plugin_basename( __FILE__ ) );
@@ -581,6 +583,25 @@ final class WC_Stellar {
 		$orders = array();
 
 		return $query->posts;
+	}
+
+	/**
+	 * Check for payments on pending orders
+	 *
+	 * @access public
+	 */
+	public function woocommerce_stellar_cron_job() {
+
+		// Fetch recent orders.
+		$order_ids = $this->get_recent_stellar_orders();
+
+		$stellar_settings = get_option( 'woocommerce_stellar_settings' );
+
+		$wallet_address = $stellar_settings['account_address'];
+
+		foreach ( $order_ids as $order_id ) {
+			WC_Stellar()->validate_stellar_payment( $order_id );
+		}
 	}
 
 	/**
