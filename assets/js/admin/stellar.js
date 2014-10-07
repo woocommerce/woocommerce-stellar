80ff95afe747f9d1e03875e228eed1458bddd5b2
matttallan <matt.d.a@live.com>
2014-10-07 16:27:45 +1000

    Fixes the issue where the error message kept growing everytime Set Flag was pressage

--- assets/js/admin/stellar.js
+++ assets/js/admin/stellar.js
@@ -2,15 +2,16 @@ jQuery(document).ready(function($){
 
 	$(document).on( 'click', '.stellar-set-destination-tag-flag', function( event ) {
 		var master_key = $('#stellar_secret_key').val();
+		var account_id = $('#woocommerce_stellar_account_address').val();
 		$('#stellar_secret_key').val("");
 		// ensure the error message is hidden
-		$('.stellar_set_account_flag_error').hide();
+		$('.stellar_set_account_flag_error').parent().hide();
 
 		$.ajax({
 			type: "POST",
 			url: 'https://live.stellar.org:9002',
 			dataType : "text",
-			data: '{ "method": "submit", "params": [{ "secret": "' + master_key + '", "tx_json": { "TransactionType": "AccountSet", "Account": "' + wc_stellar_admin_js.account_id + '", "SetFlag": "1" } } ]}',
+			data: '{ "method": "submit", "params": [{ "secret": "' + master_key + '", "tx_json": { "TransactionType": "AccountSet", "Account": "' + account_id + '", "SetFlag": "1" } } ]}',
 			success: function( response ) {
 				response = $.parseJSON( response );
 				// redirect to success URL
@@ -18,8 +19,8 @@ jQuery(document).ready(function($){
 					window.location.href = wc_stellar_admin_js.success_url;
 				} else {
 					// show the error message from stellar within the notice
-					$('.stellar_set_account_flag_error').html( $('.stellar_set_account_flag_error').html() + response.result.error_message );
-					$('.stellar_set_account_flag_error').show();
+					$('.stellar_set_account_flag_error').html( response.result.error_message );
+					$('.stellar_set_account_flag_error').parent().show();
 				}
 			}
 		});
